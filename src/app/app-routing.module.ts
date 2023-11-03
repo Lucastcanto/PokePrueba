2fee7be99e487d0299eddaed36a866b826d11173
Lucastcanto <lucastomascanto@gmail.com>
2023-11-03 11:17:22 -0300

    se agrega estilos a la tarjeta y maquetado tienda.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { PokedexComponent } from './pages/pokedex/pokedex.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { ModificarPerfilComponent } from './pages/modificar-perfil/modificar-perfil.component';
+import { TiendaComponent } from './pages/tienda/tienda.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'iniciar-sesion', pathMatch:'full'},
@@ -13,6 +14,7 @@ const routes: Routes = [
   {path: 'pokedex', component:PokedexComponent},
   {path: 'registrarse', component:SignupComponent},
   { path: 'modificar-perfil', component: ModificarPerfilComponent},
+  {path: 'tienda',component:TiendaComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { UserInfoComponent } from './shared/user-info/user-info.component';
 import { PokedexComponent } from './pages/pokedex/pokedex.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { ModificarPerfilComponent } from './pages/modificar-perfil/modificar-perfil.component';
+import { TiendaComponent } from './pages/tienda/tienda.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { ModificarPerfilComponent } from './pages/modificar-perfil/modificar-per
     PokedexComponent,
     SignupComponent,
     ModificarPerfilComponent,
+    TiendaComponent,
   ],
   imports: [
     BrowserModule,
