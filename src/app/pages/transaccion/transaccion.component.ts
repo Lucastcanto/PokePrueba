d54fb2564b32cc4f88d004d5ef643d964ff29542
Lucastcanto <lucastomascanto@gmail.com>
2023-11-05 19:05:56 -0300

    se agrega formulario de compra con su validacion

--- src/app/pages/transaccion/transaccion.component.ts
+++ src/app/pages/transaccion/transaccion.component.ts
@@ -3,6 +3,8 @@ import { Offer } from 'src/app/models/offer.model';
 import { User } from 'src/app/services/auth/user';
 import { Router } from '@angular/router';
 import { TiendaService } from 'src/app/services/tienda.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-transaccion',
@@ -15,10 +17,19 @@ export class TransaccionComponent {
   userData: string | null
   offer: Offer | null
   user: any | null
+  creditCardForm: FormGroup;
 
-  constructor(private router:Router, private tiendaService:TiendaService){
+
+  constructor(private router:Router, private tiendaService:TiendaService,private fb: FormBuilder){
     this.offerData = localStorage.getItem("compra")
     this.userData = localStorage.getItem("user")
+
+    this.creditCardForm = this.fb.group({
+      cardNumber: ['', [Validators.required, this.creditCardValidator, Validators.maxLength(16)]],
+      expirationDate: ['', [Validators.required, this.validateExpirationDate]],
+      cvv: ['', [Validators.required]],
+      cardHolderName: ['', [Validators.required]]
+    });
 
     if(this.offerData && this.userData){
       console.log(this.userData+ this.offerData)
@@ -31,27 +42,94 @@ export class TransaccionComponent {
     }
   }
 
-  comprar(){
-    if(this.offer && this.user){
-      let pokebolasTotal = this.user.pokebolas + this.offer.cantOfPokeballs
-      console.log(pokebolasTotal)
-      console.log(typeof(this.user.id))
+  validateExpirationDate(control: { value: any; }) {
+    const expirationDate = control.value;
 
-      
+    if (expirationDate) {
+      // Usamos una expresión regular para verificar el formato "MM/YY"
+      const regex = /^(0[1-9]|1[0-2])\/\d{2}$/;
+      if (!regex.test(expirationDate)) {
+        return { invalidDate: true };
+      }
+    }
 
+    return null;
+  }
+
+  comprar() {
+    if (this.offer && this.user) {
+      let pokebolasTotal = this.user.pokebolas + this.offer.cantOfPokeballs;
+  
       this.tiendaService.updatePokeballs(this.user.id.toString(), pokebolasTotal).subscribe(
-        (response)=>{
-          localStorage.setItem('user', JSON.stringify({nombre:this.user.nombre,apellido:this.user.apellido,email:this.user.email,creditos:this.user.creditos,pokebolas:pokebolasTotal,id:this.user.id}))
-          console.log(response)
-          this.router.navigate(["/tienda"])
+        (response) => {
+          localStorage.setItem('user', JSON.stringify({ nombre: this.user.nombre, apellido: this.user.apellido, email: this.user.email, creditos: this.user.creditos, pokebolas: pokebolasTotal, id: this.user.id }));
+          console.log(response);
+          this.router.navigate(["/tienda"]);
         },
-        (error)=>{
-          console.log(error)
+        (error) => {
+          console.log(error);
         }
-        )
-    }else{
-      console.log("hubo un problema")
+      );
+    } else {
+      console.log("Hubo un error.");
     }
   }
+  
 
+  get cardNumber() {
+    return this.creditCardForm.controls['cardNumber'];
   }
+
+  get expirationDate() {
+    return this.creditCardForm.controls['expirationDate'];
+  }
+
+  get cvv() {
+    return this.creditCardForm.controls['cvv'];
+  }
+
+  get cardHolderName() {
+    return this.creditCardForm.controls['cardHolderName'];
+  }
+
+  creditCardValidator(control: { value: any; }) {
+    const cardNumber = control.value;
+  
+    if (cardNumber) {
+      const cleanCardNumber = cardNumber.replace(/\D/g, '');
+  
+      if (cleanCardNumber) {
+        const cardDigits = cleanCardNumber.split('').map(Number);
+        let sum = 0;
+        let alternate = false;
+  
+        for (let i = cardDigits.length - 1; i >= 0; i--) {
+          let digit = cardDigits[i];
+  
+          if (alternate) {
+            digit *= 2;
+            if (digit > 9) {
+              digit -= 9;
+            }
+          }
+  
+          sum += digit;
+          alternate = !alternate;
+        }
+  
+        if (sum % 10 === 0) {
+          return null; // El número de tarjeta es válido según Luhn
+                      // Valid VISA: 4888524251678924   Invalid VISA: 4888524251678923
+        } else {
+          console.log("NUMERO DE TARJETA INVALIDO.")
+          return { invalidCard: true }; // El número de tarjeta es inválido según Luhn
+        }
+      }
+    }
+    return null; // Devuelve null si el campo está vacío o no contiene dígitos válidos
+  }
+
+}
+
+
+
