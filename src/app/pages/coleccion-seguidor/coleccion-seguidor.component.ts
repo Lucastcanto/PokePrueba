234f8884581f02e92755e98421d5597bfc48f6bf
Juan Cruz Castillo Martinez <castillom.juancruz@gmail.com>
2023-11-07 10:04:14 -0300

    avanzada funcion de amistades

--- src/app/pages/coleccion-seguidor/coleccion-seguidor.component.ts
+++ src/app/pages/coleccion-seguidor/coleccion-seguidor.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { CartasService } from 'src/app/services/cartas.service';
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { PokemonSpeciesService } from 'src/app/services/pokemon-species.service';
+import { Pokemon } from 'src/app/models/pokemon.model';
+import { carta } from 'src/app/models/carta.model';
 
 @Component({
   selector: 'app-coleccion-seguidor',
@@ -7,13 +12,31 @@ import { Component } from '@angular/core';
 })
 export class ColeccionSeguidorComponent {
   user: any
+  coleccion: Pokemon[] = []
 
-  constructor(){
+  constructor(private cartasService: CartasService, private pokemonService: PokemonService, private speciesService: PokemonSpeciesService){
     let data = localStorage.getItem('seguido')
     if(data){
       this.user = JSON.parse(data)
+      this.cartasService.getUserCards(this.user.id).subscribe(
+        (response)=>{
+          response.forEach(carta => {
+            this.pokemonService.getPokemon(carta.pokemonID.toString()).subscribe(
+              (response)=>{
+                this.coleccion.push(response)
+              },
+              (error)=>{
+                console.log(error)
+              }
+            )
+          });
+        },
+        (error)=>{
+          console.log(error)
+        }
+      )
     }
-    console.log(this.user)
+    
   }
 
 }
